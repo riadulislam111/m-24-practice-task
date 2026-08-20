bf9a027074e7f9c196c9963fed0204f21b4dfbca
riadulislam111 <riadulislam.bd.pabna@gmail.com>
2026-08-20 18:08:57 +0600

    first commit

---
+++ dist/problem-1.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const car = "acar";
+//# sourceMappingURL=problem-1.js.map
---
+++ src/problem-1.ts
@@ -0,0 +1,1 @@
+const car: string = "acar";
